0aafd98cc117e8d3a4a603ee42fe45855946cb6a
allenchang <iamallenchang@gmail.com>
2016-11-02 18:40:04 +0800

    test

---
+++ HK4-2/countView2.swift
@@ -0,0 +1,37 @@
+//
+//  countView2.swift
+//  HK4-2
+//
+//  Created by AllenChang on 2016/11/2.
+//  Copyright © 2016年 AllenChang. All rights reserved.
+//
+
+import UIKit
+
+class countView2: UIViewController {
+
+    @IBOutlet weak var countView2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HK4-2/countView3.swift
@@ -0,0 +1,45 @@
+//
+//  countView3.swift
+//  HK4-2
+//
+//  Created by AllenChang on 2016/11/2.
+//  Copyright © 2016年 AllenChang. All rights reserved.
+//
+
+import UIKit
+
+class countView3: UIViewController {
+
+
+    @IBOutlet weak var countLabel3: UILabel!
+    
+    var times = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated:Bool) {
+        times += 1
+        countLabel3.text = "第\(times)次"
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
